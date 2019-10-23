092d30aaa57f55be0882628b473b1bd05c80598a
wjaratlerdsiri <w.jaratlerdsiri@garvan.org.au>
2019-10-23 21:50:46 +1100

    Add aGATKdev scripts

---
+++ script/aGATK_mafft-0.1dev.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+(c) 2015 Brant Faircloth || http://faircloth-lab.org/
+All rights reserved.
+
+This code is distributed under a 3-clause BSD license. Please see
+LICENSE.txt for more information.
+
+08 March 2012 11:03 PST (-0800)
+
+phyluce_align_seqcap_align (mafft)
+
+"""
+
+import os,sys
+import tempfile
+import subprocess
+import ConfigParser
+import time
+import argparse
+
+from collections import namedtuple
+from collections import defaultdict
+
+import re
+import glob
+import shutil
+
+import pdb
+
+#pth.py
+
+def get_user_path(program, binary, package_only=False):
+    config = ConfigParser.ConfigParser()
+    # make case sensitive
+    config.optionxform = str
+    if package_only:
+        config.read(os.path.join(sys.prefix, 'config/phyluce.conf'))
+    else:
+        config.read([os.path.join(sys.prefix, 'config/phyluce.conf'), os.path.expanduser('~/.phyluce.conf')])
+    # ensure program is in list
+    pth = config.get(program, binary)
+    # expand path as necessary - replace CONDA variable placeholder
+    # with sys.prefix, otherwise default to normal path expansion
+    if pth.startswith("$CONDA"):
+        expand_pth = pth.replace("$CONDA", sys.prefix)
+    else:
+        expand_pth = os.path.abspath(os.path.expanduser(os.path.expandvars(pth)))
+    return expand_pth
+
+
+#helper.py
+
+class FullPaths(argparse.Action):
+    """Expand user- and relative-paths"""
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, os.path.abspath(os.path.expanduser(values)))
+
+
+def is_file(filename):
+    if not os.path.isfile:
+        msg = "{0} is not a file".format(filename)
+        raise argparse.ArgumentTypeError(msg)
+    else:
+        return filename
+
+
+#mafft
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="""Align locally in  FASTA file with MAFFT""",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    #input is big fasta of many taxa and many loci
+    parser.add_argument(
+        "--fasta",
+        required=True,
+        action=FullPaths,
+        type=str,
+        help="""The file containing FASTA reads associated with targted loci from """ +
+        """get_fastas_from_match_counts.py"""
+    )
+    parser.add_argument(
+        "--output",
+        required=True,
+        action=FullPaths,
+        type=str,
+        help="""The file in which to store the resulting alignments."""
+    )
+    parser.add_argument(
+        "--cores",
+        type=int,
+        default=1,
+        help="""Process alignments in parallel using --cores for alignment. """ +
+        """This is the number of PHYSICAL CPUs."""
+    )
+    return parser.parse_args()
+
+class SimpleAlign():
+    '''docstring for lastz'''
+    def __init__(self, fasta, cores, output):
+        # if not an output file, create a temp file to hold output
+        #if not out:
+            #fd, self.output = tempfile.mkstemp(suffix='.lastz')
+            #os.close(fd)
+        #else:
+        self.output = output
+        self.cli = '{3} --localpair --thread {1} \
+                --maxiterate 1000 {0} > {2}'.format(fasta, cores, self.output, get_user_path("binaries", "mafft"))
+        
+    def run(self):
+        mafft_stdout, mafft_stderr = subprocess.Popen(
+            self.cli,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE).communicate(None)
+        return mafft_stdout, mafft_stderr
+
+
+def main():
+    start_time = time.time()
+    print 'Started: ', time.strftime("%a %b %d, %Y  %H:%M:%S", time.localtime(start_time))
+    args = get_args()
+    d = SimpleAlign(args.fasta, args.cores, args.output)
+    mfstdout, mfstderr = d.run()
+    if mfstderr:
+        raise IOError(mfstderr)
+    end_time = time.time()
+    print 'Ended: ', time.strftime("%a %b %d, %Y  %H:%M:%S", time.localtime(end_time))
+    print 'Time for execution: ', (end_time - start_time)/60, 'minutes'
+
+if __name__ == '__main__':
+    main()
