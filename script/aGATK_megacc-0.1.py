74a9c2e00d4abeb23a8b6220d6407cc3f20a9f91
wjaratlerdsiri <w.jaratlerdsiri@garvan.org.au>
2019-10-24 14:09:23 +1100

    Update aGATK scripts

---
+++ script/aGATK_megacc-0.1.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+MEGA
+
+"""
+
+import os,sys
+import tempfile
+import subprocess
+import ConfigParser
+import time
+import argparse
+
+from collections import namedtuple
+from collections import defaultdict
+
+import re
+import glob
+import shutil
+#from operator import itemgetter
+
+
+#from phyluce.pth import get_user_path
+
+import pdb
+
+class SimpleMega():
+    '''docstring for lastz'''
+    def __init__(self, target, out):
+        # if not an output file, create a temp file to hold output
+        #else:
+        self.output = out
+        self.cli = '{2} -d {0} \
+                -o {1} \
+                -a data/megacc_distance.mao'.format(target, self.output, os.path.abspath("megacc"))
+#SimpleAlign() will get program binary from config file
+
+    def run(self):
+        mega_stdout, mega_stderr = subprocess.Popen(
+            self.cli,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE).communicate(None)
+        return mega_stdout, mega_stderr
+
+
+class Reader():
+    """read a lastz file and return an iterator over that file"""
+    def __init__(self, lastz_file, long_format = False):
+        self.file = open(lastz_file, 'rU')
+        self.long_format = long_format
+
+    def __del__(self):
+        """close files"""
+        self.file.close()
+
+    def __iter__( self ):
+        """iterator"""
+        while True:
+            yield self.next()
+
+
+"""
+helpers.py
+
+Created by Brant Faircloth on 15 July 2011.
+Copyright 2011 Brant C. Faircloth. All rights reserved.
+"""
+#import pdb
+#it about PATH
+class FullPaths(argparse.Action):
+    """Expand user- and relative-paths"""
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, os.path.abspath(os.path.expanduser(values)))
+
+
+def get_args():
+    """Get arguments from CLI"""
+    parser = argparse.ArgumentParser(
+        description="""Run megacc in an easy way"""
+    )
+    parser.add_argument(
+        "--target",
+        required=True,
+        type=str,
+        action=FullPaths,
+        help="""The path to the target file (2bit/fasta)"""
+    )
+    parser.add_argument(
+        "--output",
+        required=True,
+        type=str,
+        action=FullPaths,
+        help="""The path to the output file"""
+    )
+    return parser.parse_args()
+
+
+def main():
+    start_time = time.time()
+    print 'Started: ', time.strftime("%a %b %d, %Y  %H:%M:%S", time.localtime(start_time))
+    args = get_args()
+    d = SimpleMega(args.target, args.output)
+    mgstdout, mgstderr = d.run()
+    if mgstderr:
+        raise IOError(mgstderr)
+    end_time = time.time()
+    print 'Ended: ', time.strftime("%a %b %d, %Y  %H:%M:%S", time.localtime(end_time))
+    print 'Time for execution: ', (end_time - start_time)/60, 'minutes'
+
+if __name__ == '__main__':
+    main()
